2f0bd969ccd7870c6ac706739a9151cfa6b74778
Oleg Gras Magriñà <olgr786@vidalibarraquer.net>
2026-02-03 15:28:37 +0000

    Afegeix la imatge que te compartit a la carpeta public LogoNaluWater- co

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,7 +12,7 @@ export function Logo({ className }: { className?: string }) {
       )}
     >
       <Image 
-        src="/LogoNaluWater.png" 
+        src="/LogoNaluWater-.png" 
         alt="NaluWater Logo" 
         width={240} 
         height={66}
