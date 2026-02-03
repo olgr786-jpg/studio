aebfb5147acdbb26ff2c47b2485937c6c5f96a88
Oleg Gras Magriñà <olgr786@vidalibarraquer.net>
2026-02-03 15:30:42 +0000

    Vull que treguis una mica mes d'espai entre cada apartat de la pagina we

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh bg-background">
       <Header />
-      <main className="flex-1 space-y-48 sm:space-y-64">
+      <main className="flex-1 space-y-56 sm:space-y-72">
         <Hero />
         <Products />
         <WhoWeAre />
