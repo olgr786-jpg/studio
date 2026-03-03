a461cb3ed2f039c062f98562403d25af0047649f
Oleg Gras Magriñà <olgr786@vidalibarraquer.net>
2026-03-03 14:47:51 +0000

    Vull que el fons de la web es veigui negre i gris depenent en quina secc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ca" className={`${inter.variable} ${playfairDisplay.variable} !scroll-smooth`}>
+    <html lang="ca" className={`${inter.variable} ${playfairDisplay.variable} !scroll-smooth dark`}>
        <head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#00aba9" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body className={'antialiased font-sans'}>
         {children}
